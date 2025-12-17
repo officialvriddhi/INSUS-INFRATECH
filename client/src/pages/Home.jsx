8d997c7861893a8aafcc3b4d8f3ea6ad25169e77
Sameer yadav <sameer.manoj2005@gmail.com>
2025-12-17 10:21:35 +0530

    home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,6 +4,9 @@ import SplitText from '../components/SplitText.jsx';
 import { motion } from 'motion/react';
 import { NavLink, useNavigate } from 'react-router-dom';
 import React from 'react';
+import CountUp from '../components/Counter.jsx';
+import { VscWorkspaceTrusted } from "react-icons/vsc";
+import { GrUserWorker } from "react-icons/gr";
 
 const About = React.lazy(()=>import('./About.jsx'))
 
@@ -24,11 +27,11 @@ const navigate = useNavigate();
     <>
         <main className="relative z-10 flex h-full w-full items-center justify-around px-10 mb-20">
 
-            <div className='w-1/2 relative -top-5'>
+            <div className='w-1/2 relative '>
                 <h1 className='inline rounded-lg'>
                             <SplitText
                                 text="INSUS INFRATECH"
-                                className="font-bold text-slate-900 text-center text-7xl font-mono rounded-lg shadow-lg p-3 mb-5 md:text-6xl"
+                                className="font-bold text-slate-900 text-center text-4xl font-mono rounded-lg shadow-lg p-3 mb-5  lg:text-6xl md:text-6xl"
                                 delay={100}
                                 duration={0.4}
                                 ease="power3.out"
@@ -43,36 +46,88 @@ const navigate = useNavigate();
                 </h1>
 
 
-                <span className="text-rotate text-7xl w-full flex justify-end">
-                    <span className="justify-items-center">
-                        <p className='text-slate-500 font-semibold text-2xl text-right bg-gradient-to-r  from-zinc-900 via-stone-700 to-amber-600 bg-clip-text text-transparent'> -  "Strong Foundations. Smarter Infrastructure."</p>
-                        
-                        <p className='text-slate-500 font-semibold text-2xl text-right bg-gradient-to-r  from-zinc-900 via-stone-700 to-amber-600 bg-clip-text text-transparent'> -  "Building Landmarks That Stand the Test of Time"</p>
+                <motion.p 
+                initial={{opacity:0}}
+                animate={{opacity:1}}
+                transition={{duration:0.6,delay:0.2}}
+                className='text-slate-700 text-xl font-semibold bg-gradient-to-r  from-zinc-900 via-yellow-500 via-stone-700 to-amber-600 bg-clip-text text-transparent'>INSUS INFRATECH Private Limited is a trusted construction and infrastructure company delivering premium hotels, commercial spaces, and public landmarks across India since 2005.</motion.p>
 
-                        <p className='text-slate-500 font-semibold text-2xl text-right bg-gradient-to-r  from-zinc-900 via-stone-700 to-amber-600 bg-clip-text text-transparent'> - "Engineering Excellence. Building the Future"</p>
+
+                <span className="text-rotate text-7xl w-full flex justify-end mt-6">
+                    <span className="justify-items-center">
+                        <p className='text-slate-400 font-semibold text-sm text-right lg:text-[16px]'> - "A trusted construction & infrastructure company since 2005."</p>
+
+                        <p className='text-slate-400 font-semibold text-sm text-right lg:text-[16px]'> - “Delivering premium hotels, commercial spaces across India."</p>
+                        
+                        <p className='text-slate-400 font-semibold text-sm text-right lg:text-[16px]'> - "Building Landmarks That Stand the Test of Time"</p>
 
                     </span>
                 </span>
 
 
+                <div className='w-[90%] h-fit flex items-center justify-start mt-10 flex-1 gap-[2rem]'>
+
+                    <div className='w-fit items-center justify-center  px-4 py-2  text-slate-900 rounded-lg flex flex-col shadow-sm'>
+                        <div className='flex-1 text-4xl'>              
+                            <CountUp
+                            from={0}
+                            to={100}
+                            separator=","
+                            direction="up"
+                            duration={1}
+                            className="count-up-text"
+                        />+</div>
+                        <p className='text-md text-gray-400'>Project</p>
+                    </div>
+
+                    <div className='w-fit items-center justify-center  px-4 py-2 text-slate-900 rounded-lg flex flex-col shadow-sm'>
+                        <div className='flex-1 text-4xl'>              
+                            <CountUp
+                            from={0}
+                            to={19}
+                            separator=","
+                            direction="up"
+                            duration={1}
+                            className="count-up-text"
+                        />+</div>
+                        <p className='text-md text-gray-400'>Years Experience</p>
+                    </div>
+
+                    <div className='w-fit items-center justify-center  px-4 py-2 text-slate-900  rounded-lg flex flex-col shadow-sm'>
+                        <div className='flex-1 text-4xl'> 
+                            <CountUp
+                                from={0}
+                                to={10}
+                                separator=","
+                                direction="up"
+                                duration={1}
+                                className="count-up-text"
+                            />+
+                        </div>
+                        <p className='text-md text-gray-400'>Cities Served</p>
+                    </div>
+
+                    <div className='w-fit items-center justify-center  px-2 py-2 text-slate-900 rounded-lg flex flex-col shadow-sm'>
+                        <div className='flex-1 text-4xl'>              
+                          <VscWorkspaceTrusted />    
+                        </div>
+                        <p className='text-md text-gray-400'>Customer's Trusted</p>
+                    </div>
 
 
-
-                <motion.p 
-                initial={{opacity:0,x:-300}}
-                animate={{opacity:1,x:0}}
-                transition={{duration:0.6,delay:0.8}}
-                className='text-slate-700 text-lg mt-20 font-light'>INSUS INFRATECH Private Limited is a trusted construction and infrastructure company delivering premium hotels, commercial spaces, and public landmarks across India since 2005.</motion.p>
+                </div>
 
 
-                <div className='text-md text-slate-700 flex gap-10 mt-12'>
+                <div className='text-lg flex gap-10 mt-12'>
 
-                    <button onClick={()=>navigate('/work')} className='rounded-lg border border-gray-200 px-3 py-1 bg-slate-900 text-white hover:bg-white hover:text-slate-700 hover:border-slate-700 hover:border-2 transition-all duration-200 ease-in-out'>
-                        Our Projects</button>
+                    <button onClick={()=>navigate('/work')} className='flex justify-center items-center gap-2 rounded-md border px-3 py-1  text-white bg-slate-700 border-slate-700 hover:border-2transition-all duration-300 ease'>
+                        View Projects
+                        <GrUserWorker />
+                    </button>
 
 
-                    <button onClick={()=>navigate('/contact')} className='rounded-lg border border-gray-200 px-5 py-2 bg-slate-900 text-white hover:bg-white hover:text-slate-700 hover:border-slate-700 hover:border-2 transition-all duration-200 ease-in-out'>
-                        Schedule a Call
+                    <button onClick={()=>navigate('/contact')} className='rounded-md border px-3 py-1  bg-white text-slate-700 border-slate-700 hover:border-2 transition-all duration-300 ease'>
+                        Contact us
                     </button>
 
 
@@ -88,6 +143,7 @@ const navigate = useNavigate();
                         autoplayDelay={1800}
                         pauseOnHover={true}
                         sendToBackOnClick={true}
+                        className="shadow-[0_20px_60px_rgba(0,0,0,0.15)]"
                         cards={images.map((src, i) => (
                         <Image
                             key={i} 
@@ -99,7 +155,9 @@ const navigate = useNavigate();
                         ))}
                     />
                 </div>
+
         </main>
+    <hr className='w-full mx-auto h-[2px] border-0 bg-gradient-to-r from-gray-300 via-gray-500 to-gray-300'/>
         <About/>
 </>
   );
